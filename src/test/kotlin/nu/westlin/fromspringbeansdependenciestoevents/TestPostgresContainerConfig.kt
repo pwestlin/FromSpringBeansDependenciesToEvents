8236b9ae347b28c8521657c6df3558cc51724ee9
pevest <pevest@lm.se>
2025-06-04 07:36:41 +0200

    Städ

--- src/test/kotlin/nu/westlin/fromspringbeansdependenciestoevents/TestPostgresContainerConfig.kt
+++ src/test/kotlin/nu/westlin/fromspringbeansdependenciestoevents/TestPostgresContainerConfig.kt
@@ -2,7 +2,6 @@ package nu.westlin.fromspringbeansdependenciestoevents
 
 import org.springframework.boot.test.context.TestConfiguration
 
-// Utan denna fungerar testerna i IntelliJ men inte i Gradle...?
 @TestConfiguration
 class TestPostgresContainerConfig {
 
